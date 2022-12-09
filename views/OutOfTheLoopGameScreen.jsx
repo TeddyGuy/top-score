f1f60ca12ac709ab7df0dfa1bbe5568094e5df27
Teddy Guy <mediteddyguy@gmail.com>
2022-12-09 07:01:56 -0500

    additions

--- views/OutOfTheLoopGameScreen.jsx
+++ views/OutOfTheLoopGameScreen.jsx
@@ -5,12 +5,18 @@ import { GameContext } from "../App";
 import { styles, typography } from "../AppStyle";
 import { db } from "../firebaseConfig";
 import { doc, getDoc, deleteDoc, setDoc, onSnapshot } from "firebase/firestore";
+import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
+import AppButton from "../components/AppButton/AppButton";
+
+const WordBank = ["BANANA", "AIRPLANE", "ENGLISH", "HEADPHONES", "DOOR", "PIZZA", "OBAMA", "INSANE", "HAMBURGER", "POUTINE", "CANADA", "UKRAINE", "GAME OF THRONE", "RIZZ"];
 
 export const OutOfTheLoopGameScreen = ({ navigation, route }) => {
 
     const { player, otherPlayers, setOtherPlayers, setPlayer } = useContext(GameContext);
-    const [host, setHost] = useState("");
     const [countDown, setCountDown] = useState(0);
+    const [isOOTL, setIsOOTL] = useState(false);
+    const [word, setWord] = useState("")
+    const [turn, setTurn] = useState(false);
 
     useEffect(() => onSnapshot(doc(db, "rooms", route.params.roomId), (docSnap) => {
         if(!docSnap.exists()){
@@ -26,8 +32,67 @@ export const OutOfTheLoopGameScreen = ({ navigation, route }) => {
         if(player) {
             setOtherPlayers(room.players.filter((otherPlayer) => player.name !== otherPlayer.name ));
         }
+    }), []);
 
-        setHost(room.host)
+    useEffect(() => onSnapshot(doc(db, "rooms", route.params.roomId), (docSnap) => {
+        if(!docSnap.exists()){
+            return;
+        }
+
+        const room = docSnap.data();
+        
+        setIsOOTL(player.name === room.isOOTL); 
+        setWord(room.OOTLWord);
+    }), []);
+
+    useEffect(() => onSnapshot(doc(db, "rooms", route.params.roomId), (docSnap) => {
+        if(!docSnap.exists()){
+            return;
+        }
+
+        const room = docSnap.data();
+        
+        if(!room.isOOTL || !room.OOTLWord){
+            return;
+        }
+        
+        setTurn(room.turn)
+    }), []);
+
+    useEffect(() => onSnapshot(doc(db, "rooms", route.params.roomId), async (docSnap) => {
+        if(!docSnap.exists()){
+            return;
+        }
+
+        const room = docSnap.data();
+        
+        let everyoneIsReady = true; 
+        room.players.forEach(player => {
+            if(player.state !== "Ready") everyoneIsReady = false;
+        });
+
+        if(!everyoneIsReady){
+            return;
+        }
+
+        if(room.host !== player.name){
+            return;
+        }
+
+        if(room.isOOTL && room.OOTLWord){
+            return;
+        }
+
+        const docRef = doc(db, "rooms", route.params.roomId);
+        const chosenPlayer = room.players[Math.floor(Math.random() * room.players.length)].name;
+        await setDoc(docRef,
+            {
+                isOOTL: chosenPlayer,
+                OOTLWord: WordBank[Math.floor(Math.random() * room.players.length)],
+                turn: room.players.filter(player => player.name !== chosenPlayer)[Math.floor(Math.random() * room.players.length)].name
+            },
+            {merge: true});
+
     }), []);
 
     useEffect(() => onSnapshot(doc(db, "rooms", route.params.roomId), async (docSnap) => {
@@ -46,7 +111,7 @@ export const OutOfTheLoopGameScreen = ({ navigation, route }) => {
             navigation.navigate("Home");
         }
 
-        if(playerWithNameFound.state === "Ready") {
+        if(playerWithNameFound.state === "Ready" || playerWithNameFound.state === "Played") {
             return;
         }
 
@@ -66,14 +131,13 @@ export const OutOfTheLoopGameScreen = ({ navigation, route }) => {
             await setDoc(docRef, {players: [...otherPlayers, {...player, state: "Ready"}]}, {merge: true})
         }
 
-        if(countDown !== 0)  return;
-
+        if(countDown > 0)  return;
         updateDoc()
 
     },[countDown])
 
     useEffect(() => {
-        if(countDown === 0) {
+        if(countDown < 1) {
             return;
         }
 
@@ -92,7 +156,11 @@ export const OutOfTheLoopGameScreen = ({ navigation, route }) => {
     <View style={styles.container}>
         {countDown !== 0 && <Text style={typography.header}>GAME STARTING IN</Text>} 
         {countDown !== 0 && <Text style={typography.header2}>{countDown}</Text>}
-        {countDown === 0 && <Text style={typography.header}>LETS GO</Text>}
+        {countDown === 0 && isOOTL && <Text style={typography.header}>YOU'RE OUT OF THE LOOP</Text>}
+        {countDown === 0 && !isOOTL && <Text style={typography.header}>THE WORD IS :</Text>}
+        {countDown === 0 && !isOOTL && <Text style={typography.header2}>{word}</Text>}
+        {turn === player.name && <AppButton title={"END YOUR TURN"}/>}
+        {countDown === 0 && turn !== player.name && <Text style={typography.header2}>It's {turn}'s turn ...</Text>}
     </View>
     );
 }
